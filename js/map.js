ba4cf6213ddd6c7315d8d3b24466378f69e73796
Derek Eder <derek.eder@gmail.com>
2014-05-07 18:01:34 -0500

    added more stuff to tooltip, started on jenks for coloring map

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
     var lastClicked;
     var boundaries;
     var map = L.map('map').fitBounds([[36.970298, -87.495199],[42.5083380000001,-91.5130789999999]]);
-    L.tileLayer('https://{s}.tiles.mapbox.com/v3/datamade.hnmob3j3/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/datamade.hn83a654/{z}/{x}/{y}.png', {
         attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
     }).addTo(map);
     var info = L.control({position: 'bottomleft'});
@@ -18,32 +18,55 @@
             info.removeFrom(map);
         }
     }
+
+    var map_colors = [
+        '#c7e9c0',
+        '#a1d99b',
+        '#74c476',
+        '#31a354',
+        '#006d2c'
+    ]
+
+    var jenks_cutoffs = []
     $.when($.getJSON('data/finished_files/merged_eitc.geojson')).then(
         function(shapes){
             boundaries = L.geoJson(shapes, {
                 style: style,
                 onEachFeature: boundaryClick
             }).addTo(map);
+
+            var all_values = []
+            boundaries.eachLayer(function(s){
+                all_values.push(s.feature.properties['PCTEITC'] * 100);
+            });
+            jenks_cutoffs = jenks(all_values, 4);
+            jenks_cutoffs[0] = 0; // ensure the bottom value is 0
+            jenks_cutoffs.pop(); // last item is the max value, so dont use it
+            console.log(jenks_cutoffs);
+
+            //set the color style here!!
         }
     );
     function style(feature){
         var style = {
-            "color": "#000",
+            "color": getColor(feature.properties['PCTEITC'] * 100),
             "opacity": 0.5,
             "weight": 1,
             "fillOpacity": 0.3,
         }
-        //if (feature.properties['COUNT'] < 90){
-        //    style['fillColor'] = "#a6dba0"
-        //}
-        //if (feature.properties['COUNT'] > 90 && feature.properties['COUNT'] < 200){
-        //    style['fillColor'] = "#5aae61";
-        //}
-        //if (feature.properties['COUNT'] > 200){
-        //    style['fillColor'] = "#1b7837";
-        //}
         return style;
     }
+
+    // get color depending on condition_title
+    function getColor(d) {
+        console.log(d)
+        return  d >= jenks_cutoffs[3] ? map_colors[0] : 
+                d >= jenks_cutoffs[2] ? map_colors[1] : 
+                d >= jenks_cutoffs[1] ? map_colors[2] : 
+                d >= jenks_cutoffs[0] ? map_colors[3] :  
+                                        map_colors[4];
+    }
+
     function boundaryClick(feature, layer){
         layer.on('click', function(e){
             if(typeof lastClicked !== 'undefined'){
@@ -63,15 +86,37 @@
         })
     }
     function featureInfo(properties){
-        var blob = '<div><h3>District ' + parseInt(properties['ILHOUSEDIS']) + '</h3>';
-        blob += '<p><strong>Representative: </strong>' + properties['HOUSEREP'] + ' (' + properties['PARTY'] + ')</p>';
-        blob += '<p><strong>EITC Returns: </strong>' + properties['EITC'] + ' (' + Math.round(properties['PCTEITC'] * 100) + '%)</p>';
-        //blob += '<p><strong>: </strong>' + accounting.formatMoney(properties['BACK_TAXES']) + '</p>';
-        //blob += '<p><strong>Property Status: </strong>' + properties['PROPERTY_S'] + '</p>';
-        //blob += '<p><strong>Demolition Estimate: </strong>' + accounting.formatMoney(properties['CITY_ESTIM']) + '</p>';
-        //blob += '<p><strong>Neighborhood: </strong>' + properties['NEIGHBORHO'] + '</p>';
-        //blob += '<p><strong>Council District: </strong>' + properties[' COUNCIL_D'] + '</p>';
-        blob += '</div>';
+        var blob = '<div>\
+            <h3>District ' + parseInt(properties['ILHOUSEDIS']) + '</h3>\
+            <table>\
+              <tbody>\
+                <tr>\
+                    <td>Representative</td>\
+                    <td>' + properties['HOUSEREP'] + '</td>\
+                </tr>\
+                <tr>\
+                    <td>EITC returns</td>\
+                    <td>' + properties['EITC'] + ' (' + Math.round(properties['PCTEITC'] * 100) + '%)</td>\
+                </tr>\
+                <tr>\
+                    <td>Party</td>\
+                    <td>' + properties['PARTY'] + '</td>\
+                </tr>\
+                <tr>\
+                    <td>EITC 10% (current)</td>\
+                    <td>' + properties['AVGEITC10'] + '</td>\
+                </tr>\
+                <tr>\
+                    <td>EITC 20% (expanded)</td>\
+                    <td>' + properties['AVGEITC20'] + '</td>\
+                </tr>\
+                <tr>\
+                    <td>ETIC multiplier</td>\
+                    <td>' + accounting.formatMoney(properties['MULTEFFECT']) + 'M</td>\
+                </tr>\
+              </tbody>\
+            </table>\
+            </div>';
         return blob
     }
 })()
